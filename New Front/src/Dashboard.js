858079a6537a7acc4b7f943542d0f9b73e000a04
Mahmoud-Elsheikh <46035641+Mahmoud-Elsheikh@users.noreply.github.com>
2020-04-22 18:09:15 +0400

    Added New Frontend

---
+++ New Front/src/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import Button from '@material-ui/core/Button';
+
+export default () => (
+    <Card>
+        <Button variant="contained" color="primary">
+        Hello World
+        </Button>
+        <CardHeader title="Welcome to the administration" />
+        <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+    </Card>
+);
---
+++ New Front/src/Houses.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { List, Datagrid, TextField } from 'react-admin';
+
+export const HouseList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+        </Datagrid>
+    </List>
+);
+
+export const RoomList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+        </Datagrid>
+    </List>
+);
