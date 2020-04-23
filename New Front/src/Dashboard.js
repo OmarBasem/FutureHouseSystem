82d56fbc632293f98c8ab43d5f7e539c3f452c51
Mahmoud-Elsheikh <46035641+Mahmoud-Elsheikh@users.noreply.github.com>
2020-04-23 12:06:58 +0400

    Add files via upload

--- New Front/src/Dashboard.js
+++ New Front/src/Dashboard.js
@@ -3,15 +3,6 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import Button from '@material-ui/core/Button';
-import 
-
-const cardStyle = {
-    width: 300,
-    minHeight: 300,
-    margin: '0.5em',
-    display: 'inline-block',
-    verticalAlign: 'top'
-};
 
 export default () => (
     <Card>
